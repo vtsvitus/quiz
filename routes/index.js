ddd5dc9f921b596e12353b00d1e915f1beabf0a5
vtsvitus <vitus_vitus@hotmail.com>
2015-06-12 13:03:21 +0200

    Ejercio Obligatorio

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+	res.render('author', { name: 'David Frias' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
 
