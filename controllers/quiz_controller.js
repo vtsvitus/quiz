c5df4eaa56d0740cce22dd7de5e5ef007c9465d8
David Frías <vitus_vitus@hotmail.com>
2015-06-28 19:42:52 +0200

    Ejercicio Obligatorio2

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,19 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index', { quizes: quizes});
-    }
-  ).catch(function(error) { next(error);})
+  if (req.query.search) {
+  var search = '%' + req.query.search.replace(/\s/g,"%") + '%';
+  models.Quiz.findAll({
+	where: ["pregunta like ?", search],
+	order: [['pregunta', 'ASC']]}
+	).then(function(quizes) {	
+	res.render('quizes/index', {quizes: quizes});
+}).catch(function(error) {next(error);});
+  }else{
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index', {quizes: quizes});
+	}).catch(function(error) {next(error);});
+  }
 };
 
 // GET /quizes/:id
