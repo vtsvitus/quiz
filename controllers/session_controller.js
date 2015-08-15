39f830484c857a7d04a9afa6e16505ef71d08a9c
David Frías <vitus_vitus@hotmail.com>
2015-08-15 14:23:33 +0200

    Autorizacion

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,4 +1,13 @@
-﻿// Get /login   -- Formulario de login
+﻿// MW de autorización de accesos HTTP restringidos
+exports.loginRequired = function(req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
+
+// Get /login   -- Formulario de login
 exports.new = function(req, res) {
     var errors = req.session.errors || {};
     req.session.errors = {};
